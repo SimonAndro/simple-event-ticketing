8b315bfba3b06a11035b42ee3dcb3342d477a399
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-10-21 20:09:36 +0800

    untrack config file

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return[
-    "base_url" => "http://localhost/simple-event-ticketing",
-    "data_file_name" => __DIR__ . '/data/aeb3bcb1b6148eb24c6917029b604291.xlsx',
-    "spin_pass" => "1234"
-];
